0c57e188b0248375a96dcb6f5165459be7d0b987
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-08-05 22:00:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -1,13 +1,13 @@
 class Solution {
   public:
-    int solve(int i,int j,vector<int> &arr,vector<vector<int>>&dp){
+    int solve(vector<int> &arr,int i,int j,vector<vector<int>>&dp){
         // base case
         if(i==j) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
         int minAns=1e9;
         for(int k=i;k<j;k++){
-            int curr=arr[i-1]*arr[k]*arr[j]+solve(i,k,arr,dp)+solve(k+1,j,arr,dp);
-            minAns=min(minAns,curr);
+            int steps=arr[i-1]*arr[k]*arr[j]+solve(arr,i,k,dp)+solve(arr,k+1,j,dp);
+            minAns=min(minAns,steps);
         }
         dp[i][j]=minAns;
         return dp[i][j];
@@ -15,8 +15,8 @@ class Solution {
     int matrixMultiplication(vector<int> &arr) {
         // code here
         int n=arr.size();
-        vector<vector<int>>dp(n+1,vector<int>(n,-1));
-        int ans=solve(1,n-1,arr,dp);
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        int ans=solve(arr,1,n-1,dp);
         return ans;
     }
 };
