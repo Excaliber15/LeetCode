25c7cd2c7f71823604b245dc1534f8e37a77fd31
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-08-06 22:55:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (83.36%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            char ch=s[i];
+            if(ch=='(' || ch=='{' || ch=='['){
+                // opening brackets
+                st.push(ch);
+            }
+            else{
+                // closing brackets
+                if(!st.empty()){
+                    if(ch==')' && st.top()=='('){
+                        st.pop();
+                    }
+                    else if(ch=='}' && st.top()=='{'){
+                        st.pop();
+                    }
+                    else if(ch==']' && st.top()=='['){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    // invalid case
+                    return false;
+                }
+            }
+        }
+        if(st.size()==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
