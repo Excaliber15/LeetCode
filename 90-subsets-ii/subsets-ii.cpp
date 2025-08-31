e10d5c3fbfe968cd832cde7fa71011addb584af7
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-08-31 16:36:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (66.75%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -6,16 +6,18 @@ public:
             ans.push_back(v);
             return;
         }
-        // include case
+        // include ans
         v.push_back(nums[i]);
         printAllSubsets(nums,ans,v,i+1);
         v.pop_back();
         int idx=i+1;
         while(idx<nums.size() && nums[idx]==nums[idx-1]) idx++;
-        // exclude case
+        // exclude ans
         printAllSubsets(nums,ans,v,idx);
+
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        // sort the array
         sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
         vector<int>v;
