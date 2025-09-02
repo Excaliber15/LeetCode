89909114836e10e8f1695fdc9e378ce02d1433c8
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-02 09:33:12 +0530

    Time: 2 ms (42.00%) | Memory: 10.4 MB (84.77%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void printUniqueSubsets(vector<int>& nums,vector<vector<int>>&ans,vector<int>&v,int i){
-        // base cases
+    void printAllSubsets(vector<int>& nums,vector<vector<int>>&ans,vector<int>&v,int i){
+        // base case
         if(i>=nums.size()){
             ans.push_back(v);
             return;
         }
         // include ans
         v.push_back(nums[i]);
-        printUniqueSubsets(nums,ans,v,i+1);
+        printAllSubsets(nums,ans,v,i+1);
         v.pop_back();
+        // exclude ans
         int idx=i+1;
         while(idx<nums.size() && nums[idx]==nums[idx-1]) idx++;
-        // exclude ans
-        printUniqueSubsets(nums,ans,v,idx);
+        printAllSubsets(nums,ans,v,idx);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
         vector<int>v;
         int index=0;
-        printUniqueSubsets(nums,ans,v,index);
+        printAllSubsets(nums,ans,v,index);
         return ans;
     }
 };
