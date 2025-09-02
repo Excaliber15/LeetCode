af603a8a82f37cadedf64928603f4d7ce63a15f0
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-02 09:11:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sums/subset-sums.cpp
+++ Difficulty: Medium/Subset Sums/subset-sums.cpp
@@ -1,22 +1,22 @@
 class Solution {
   public:
-    void printAllSubsetsSums(vector<int>& arr,vector<int>&ans,int i,int sum){
+    void printAllSubsetSums(vector<int>& arr,vector<int>&ans,int i,int sum){
         // base case
         if(i>=arr.size()){
             ans.push_back(sum);
             return;
         }
         // include ans
-        printAllSubsetsSums(arr,ans,i+1,sum+arr[i]);
+        printAllSubsetSums(arr,ans,i+1,sum+arr[i]);
         // exclude ans
-        printAllSubsetsSums(arr,ans,i+1,sum);
+        printAllSubsetSums(arr,ans,i+1,sum);
     }
     vector<int> subsetSums(vector<int>& arr) {
         // code here
         vector<int>ans;
         int index=0;
         int sum=0;
-        printAllSubsetsSums(arr,ans,index,sum);
+        printAllSubsetSums(arr,ans,index,sum);
         return ans;
     }
 };
