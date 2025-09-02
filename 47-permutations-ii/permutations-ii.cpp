db44bc24a30537492a658eaaca6d4b2a486e8e0e
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-02 11:48:16 +0530

    Time: 93 ms (8.71%) | Memory: 35.7 MB (6.58%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void printUniquePermu(vector<int>& nums,vector<vector<int>>&ans,int i){
+    void printAllPermutations(vector<int>& nums,vector<vector<int>>&ans,int i){
         // base case
         if(i>=nums.size()){
             ans.push_back(nums);
@@ -8,14 +8,14 @@ public:
         }
         for(int j=i;j<nums.size();j++){
             swap(nums[i],nums[j]);
-            printUniquePermu(nums,ans,i+1);
+            printAllPermutations(nums,ans,i+1);
             swap(nums[i],nums[j]); // backTrack
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>>ans;
         int index=0;
-        printUniquePermu(nums,ans,index);
+        printAllPermutations(nums,ans,index);
         sort(ans.begin(),ans.end());
         ans.erase(unique(ans.begin(),ans.end()),ans.end()); // remove duplicates
         return ans;
