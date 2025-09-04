e90799d0f627a3e45244395a94a11ed61e3b3608
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-04 21:32:53 +0530

    Time: 22 ms (46.65%) | Memory: 32.8 MB (24.26%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int coinChange_helper(int amount, vector<int>& coins,int n,int index,vector<vector<int>>&dp){
+        // base case
+        if(index==0){
+            return (amount % coins[0]==0);
+        }
+        if(dp[amount][index]!=-1) return dp[amount][index];
+        int excludeAns=0+coinChange_helper(amount,coins,n,index-1,dp);
+        int includeAns=0;
+        if(coins[index]<=amount){
+            includeAns=coinChange_helper(amount-coins[index],coins,n,index,dp);
+        }
+        int finalAns=excludeAns+includeAns;
+        dp[amount][index]=finalAns;
+        return dp[amount][index];
+    }
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        int index=n-1;
+        vector<vector<int>>dp(amount+1,vector<int>(n,-1));
+        int ans=coinChange_helper(amount,coins,n,index,dp);
+        return ans;
+    }
+};
