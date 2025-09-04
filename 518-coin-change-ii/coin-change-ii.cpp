4c71ebd4252cff9b305f79145c2fd28859c3c9ec
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-04 22:02:23 +0530

    Time: 25 ms (39.87%) | Memory: 32.9 MB (23.92%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int coinChange_Solver(int amount, vector<int>& coins,int n,int index,vector<vector<int>>&dp){
+    int coinChange_helper(int amount, vector<int>& coins,int n,int index,vector<vector<int>>&dp){
         // base case
         if(index==0){
-            return (amount % coins[0]==0);
+            return (amount%coins[0]==0);
         }
         if(dp[amount][index]!=-1) return dp[amount][index];
-        int excludeAns=0+coinChange_Solver(amount,coins,n,index-1,dp);
+        int excludeAns=coinChange_helper(amount,coins,n,index-1,dp);
         int includeAns=0;
         if(coins[index]<=amount){
-            includeAns=coinChange_Solver(amount-coins[index],coins,n,index,dp);
+            includeAns=coinChange_helper(amount-coins[index],coins,n,index,dp);
         }
         int finalAns=includeAns+excludeAns;
         dp[amount][index]=finalAns;
@@ -19,7 +19,7 @@ public:
         int n=coins.size();
         int index=n-1;
         vector<vector<int>>dp(amount+1,vector<int>(n,-1));
-        int ans=coinChange_Solver(amount,coins,n,index,dp);
+        int ans=coinChange_helper(amount,coins,n,index,dp);
         return ans;
     }
 };
