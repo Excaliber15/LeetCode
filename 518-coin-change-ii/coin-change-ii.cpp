998f3c553aa04253f9856625c01a733cd23d2f70
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-05 17:21:09 +0530

    Time: 28 ms (31.87%) | Memory: 32.8 MB (24.26%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    int coinChange_helper(int amount, vector<int>& coins,int n,int index,vector<vector<int>>&dp){
-        // base case
-        if(index==0){
-            return (amount%coins[0]==0);
-        }
-        if(dp[amount][index]!=-1) return dp[amount][index];
-        int excludeAns=coinChange_helper(amount,coins,n,index-1,dp);
+    int coinChange_helper(int amount, vector<int>& coins,int i,vector<vector<int>>&dp){
+        // basec case
+        if(i==0) return (amount % coins[0]==0);
+        if(dp[amount][i]!=-1) return dp[amount][i];
+        int excludeAns=coinChange_helper(amount,coins,i-1,dp);
         int includeAns=0;
-        if(coins[index]<=amount){
-            includeAns=coinChange_helper(amount-coins[index],coins,n,index,dp);
+        if(coins[i]<=amount){
+            includeAns=coinChange_helper(amount-coins[i],coins,i,dp);
         }
         int finalAns=includeAns+excludeAns;
-        dp[amount][index]=finalAns;
-        return dp[amount][index];
+        dp[amount][i]=finalAns;
+        return dp[amount][i];
     }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
         int index=n-1;
         vector<vector<int>>dp(amount+1,vector<int>(n,-1));
-        int ans=coinChange_helper(amount,coins,n,index,dp);
-        return ans;
+        return coinChange_helper(amount,coins,index,dp);
     }
 };
