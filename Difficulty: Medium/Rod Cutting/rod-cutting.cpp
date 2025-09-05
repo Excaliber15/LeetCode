634fc11e428aca605446a22631df9894ae08b16a
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-05 17:12:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.cpp
+++ Difficulty: Medium/Rod Cutting/rod-cutting.cpp
@@ -2,26 +2,25 @@
 
 class Solution {
   public:
-    int rodCutting_helper(vector<int> &price,int n,int index,vector<vector<int>>&dp){
+    int rodCutting_helper(vector<int> &price,int n,int i,vector<vector<int>>&dp){
         // base case
-        if(index==0) return n*price[0];
-        if(dp[n][index]!=-1) return dp[n][index];
-        int excludeAns=0+rodCutting_helper(price,n,index-1,dp);
+        if(i==0) return n*price[0];
+        if(dp[n][i]!=-1) return dp[n][i];
+        int excludeAns=0+rodCutting_helper(price,n,i-1,dp);
         int includeAns=INT_MIN;
-        int rodLength=index+1;
+        int rodLength=i+1;
         if(rodLength<=n){
-            includeAns=price[index]+rodCutting_helper(price,n-rodLength,index,dp);
+            includeAns=price[i]+rodCutting_helper(price,n-rodLength,i,dp);
         }
         int finalAns=max(includeAns,excludeAns);
-        dp[n][index]=finalAns;
-        return dp[n][index];
+        dp[n][i]=finalAns;
+        return dp[n][i];
     }
     int cutRod(vector<int> &price) {
         // code here
         int n=price.size();
         int index=n-1;
         vector<vector<int>>dp(n+1,vector<int>(n,-1));
-        int ans=rodCutting_helper(price,n,index,dp);
-        return ans;
+        return rodCutting_helper(price,n,index,dp);
     }
 };
