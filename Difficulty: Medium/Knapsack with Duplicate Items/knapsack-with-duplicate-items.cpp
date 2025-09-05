59ebf21528545c2a4e54a4ed9efd39d385f501d2
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-05 17:55:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -0,0 +1,25 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int knapSack_Solver(vector<int>& val, vector<int>& wt, int capacity, int index,vector<vector<int>>&dp){
+        // base case
+        if(index==0) return (capacity/wt[0])*val[0];
+        if(dp[capacity][index]!=-1) return dp[capacity][index];
+        int excludeAns=0+knapSack_Solver(val,wt,capacity,index-1,dp);
+        int includeAns=0;
+        if(wt[index]<=capacity){
+            includeAns=val[index]+knapSack_Solver(val,wt,capacity-wt[index],index,dp);
+        }
+        int finalAns=max(includeAns,excludeAns);
+        dp[capacity][index]=finalAns;
+        return dp[capacity][index];
+    }
+    int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
+        // code here
+        int n=val.size();
+        int index=n-1;
+        vector<vector<int>>dp(capacity+1,vector<int>(n,-1));
+        return knapSack_Solver(val,wt,capacity,index,dp);
+    }
+};
