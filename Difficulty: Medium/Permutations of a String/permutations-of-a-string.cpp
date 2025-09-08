dd568323b4711f49673f8d8cb007a046c756594c
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-08 22:32:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Permutations of a String/permutations-of-a-string.cpp
+++ Difficulty: Medium/Permutations of a String/permutations-of-a-string.cpp
@@ -1,22 +1,22 @@
 class Solution {
   public:
-    void printAllPermutations(string &s,vector<string>&ans,int i){
-        // base case
+    void printAllPermutaions(string &s,vector<string>&ans,int i){
+        // base caes
         if(i>=s.size()){
             ans.push_back(s);
             return;
         }
-        for(int j=i;j<s.length();j++){
+        for(int j=i;j<s.size();j++){
             swap(s[i],s[j]);
-            printAllPermutations(s,ans,i+1);
-            swap(s[i],s[j]); // backTracking
+            printAllPermutaions(s,ans,i+1);
+            swap(s[i],s[j]);
         }
     }
     vector<string> findPermutation(string &s) {
         // Code here there
         vector<string>ans;
         int index=0;
-        printAllPermutations(s,ans,index);
+        printAllPermutaions(s,ans,index);
         sort(ans.begin(),ans.end());
         ans.erase(unique(ans.begin(),ans.end()),ans.end());
         return ans;
