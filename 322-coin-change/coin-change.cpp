a6e7fd2347dfc63faa6ddfcc52d4bc680ac82b83
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-08 23:26:21 +0530

    Time: 479 ms (5.02%) | Memory: 99.8 MB (5.07%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    int coinChange_helper(vector<int>& coins, int amount,vector<int>&dp){
-        // base cases
+    int coinChange_helper(vector<int>& coins, int amount,int i,vector<vector<int>>&dp){
+        // base case
         if(amount==0) return 0;
         if(amount<0) return INT_MAX;
-        if(dp[amount]!=-1) return dp[amount];
+        if(dp[amount][i]!=-1) return dp[amount][i];
         int minCoinAns=INT_MAX;
         for(int i=0;i<coins.size();i++){
             int coin=coins[i];
             if(coin<=amount){
-                int recAns=coinChange_helper(coins,amount-coin,dp);
+                int recAns=coinChange_helper(coins,amount-coin,i+1,dp);
                 if(recAns!=INT_MAX){
                     int minCoinUsed=1+recAns;
                     minCoinAns=min(minCoinAns,minCoinUsed);
                 }
             }
         }
-        dp[amount]=minCoinAns;
-        return dp[amount];
+        dp[amount][i]=minCoinAns;
+        return dp[amount][i];
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,-1);
-        int ans=coinChange_helper(coins,amount,dp);
+        int n=coins.size();
+        int index=0;
+        vector<vector<int>>dp(amount+1,vector<int>(n+1,-1));
+        int ans=coinChange_helper(coins,amount,index,dp);
         if(ans==INT_MAX) return -1;
         return ans;
     }
