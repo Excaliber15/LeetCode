8b316b6f66d5c00ad0d8548654b94ef42bdb2231
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-11 17:14:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (71.62%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    int getQuotient(long long int dividend,long long int divisor){
+    int getQuotient(long long int dividend, long long int divisor){
         long long int lo=-dividend,hi=dividend,ans=-1;
         while(lo<=hi){
             long long int mid=lo+((hi-lo)>>1);
-            if((mid*divisor)==dividend) return mid;
-            else if((mid*divisor)<dividend){
+            if(divisor*mid==dividend) return mid;
+            else if(divisor*mid<dividend){
                 ans=mid;
                 lo=mid+1;
             }
-            else hi=mid-1;
+            else hi=mid-1; // divisor*mid>dividend
         }
         return ans;
     }
     int divide(int dividend, int divisor) {
-        if (dividend == INT_MIN && divisor == -1) return INT_MAX;
-        long long int ans=getQuotient(llabs((long long)dividend), llabs((long long)divisor));
-        if((dividend < 0) ^ (divisor < 0)){
-            ans = -ans;
+        if(dividend==INT_MIN && divisor==-1) return INT_MAX;
+         //int ans=getQuotient(llabs(long long)(dividend),llabs(long long)divisor));
+         long long int ans = getQuotient(llabs(dividend), llabs(divisor));
+
+        if((dividend<0 && divisor>0) || (dividend>0 && divisor<0)){
+            ans=0-ans;
         }
         return ans;
     }
