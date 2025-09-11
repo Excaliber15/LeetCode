4646130a7bfe31f59d3989153ae3d3c62538a2d0
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-11 16:58:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (99.12%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int mySqrt(int x) {
+        // Search Space Pattern
         int lo=0,hi=x,ans=-1;
         while(lo<=hi){
             long long int mid=lo+(hi-lo)/2;
             if(mid*mid==x) return mid;
             else if(mid*mid<x){
-                ans=mid; // store
-                lo=mid+1; // compute
+                ans=mid;
+                lo=mid+1;
             }
             else hi=mid-1; // mid*mid>x
         }
