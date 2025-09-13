97e4881257322e820469f72c117871ce3045f962
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-13 20:09:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,6 +1,6 @@
 class Solution {
   public:
-    int knapSack_solver(int capacity,vector<int> &val, vector<int> &wt,int index,vector<vector<int>>&dp){
+    int knapSack_helper(int capacity,vector<int> &val, vector<int> &wt,int index,vector<vector<int>>&dp){
         // base case
         if(index==0){
             if(wt[0]<=capacity){
@@ -11,10 +11,10 @@ class Solution {
             }
         }
         if(dp[capacity][index]!=-1) return dp[capacity][index];
-        int excludeAns=0+knapSack_solver(capacity,val,wt,index-1,dp);
+        int excludeAns=0+knapSack_helper(capacity,val,wt,index-1,dp); // exclude ans
         int includeAns=0;
         if(wt[index]<=capacity){
-            includeAns=val[index]+knapSack_solver(capacity-wt[index],val,wt,index-1,dp);
+            includeAns=val[index]+knapSack_helper(capacity-wt[index],val,wt,index-1,dp); // include ans
         }
         int finalAns=max(includeAns,excludeAns);
         dp[capacity][index]=finalAns;
@@ -23,10 +23,10 @@ class Solution {
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
         // code here
         int n=val.size();
-        int index=n-1;
         int capacity=W;
+        int index=n-1;
         vector<vector<int>>dp(capacity+1,vector<int>(n,-1));
-        int ans=knapSack_solver(capacity,val,wt,index,dp);
+        int ans=knapSack_helper(capacity,val,wt,index,dp);
         return ans;
     }
 };
