9a077a2c4645929e5c9065a2fe6c92a04f112efe
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-13 17:29:23 +0530

    Time: 4 ms (25.12%) | Memory: 14.1 MB (36.32%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     void combinationSum2_helper(vector<int>& candidates, int target,vector<vector<int>>&ans,vector<int>&v,int index){
-        // base case
+        // base cases
         if(target==0){
             ans.push_back(v);
             return;
         }
         if(target<0) return;
-        
+
         for(int i=index;i<candidates.size();i++){
             if(i>index && candidates[i]==candidates[i-1]) continue;
             v.push_back(candidates[i]);
@@ -16,7 +16,7 @@ public:
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
+        sort(candidates.begin(),candidates.end()); // sort the array
         vector<vector<int>>ans;
         vector<int>v;
         int index=0;
