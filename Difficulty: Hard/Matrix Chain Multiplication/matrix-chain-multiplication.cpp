919882dbb93f77c40a38316c7e30aa5b82e1bc64
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-13 22:20:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -1,12 +1,13 @@
 class Solution {
   public:
     int f(vector<int> &arr,int i,int j,vector<vector<int>>&dp){
+        // base case
         if(i==j) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
         int minAns=1e9;
         for(int k=i;k<j;k++){
             int steps=arr[i-1]*arr[k]*arr[j]+f(arr,i,k,dp)+f(arr,k+1,j,dp);
-            minAns=min(minAns,steps);
+            minAns=min(steps,minAns);
         }
         dp[i][j]=minAns;
         return dp[i][j];
