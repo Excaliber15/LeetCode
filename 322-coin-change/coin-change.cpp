9700fe19a13139856d2d26f2eb46878079f4da5b
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-14 16:11:52 +0530

    Time: 34 ms (57.79%) | Memory: 18.9 MB (63.92%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int coinChange_helper(vector<int>& coins, int amount,vector<int>&dp){
-        // base cases
+    int coinChange_solver(vector<int>& coins, int amount,int i,vector<int>&dp){
+        // base case
         if(amount==0) return 0;
         if(amount<0) return INT_MAX;
         if(dp[amount]!=-1) return dp[amount];
         int minCoinAns=INT_MAX;
         for(int i=0;i<coins.size();i++){
             if(coins[i]<=amount){
-                int recAns=coinChange_helper(coins,amount-coins[i],dp);
+                int recAns=coinChange_solver(coins,amount-coins[i],i+1,dp);
                 if(recAns!=INT_MAX){
                     int minCoinUsed=1+recAns;
                     minCoinAns=min(minCoinAns,minCoinUsed);
@@ -20,8 +20,9 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
+        int index=0;
         vector<int>dp(amount+1,-1);
-        int ans=coinChange_helper(coins,amount,dp);
+        int ans=coinChange_solver(coins,amount,index,dp);
         if(ans==INT_MAX) return -1;
         return ans;
     }
