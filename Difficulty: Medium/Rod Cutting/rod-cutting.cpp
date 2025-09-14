9ade2ca87d7025077690c559ab9fa0e8496d8ce1
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-14 16:46:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.cpp
+++ Difficulty: Medium/Rod Cutting/rod-cutting.cpp
@@ -2,21 +2,21 @@
 
 class Solution {
   public:
-    
     int rodCutting_helper(vector<int> &price,int n,int i,vector<vector<int>>&dp){
         // base case
         if(i==0) return (n*price[0]);
         if(dp[n][i]!=-1) return dp[n][i];
+        // exclude ans
         int excludeAns=0+rodCutting_helper(price,n,i-1,dp);
+        // include ans
         int includeAns=INT_MIN;
         int rodLength=i+1;
         if(rodLength<=n){
             includeAns=price[i]+rodCutting_helper(price,n-rodLength,i,dp);
         }
-        int finalAns=max(includeAns,excludeAns);
-        dp[n][i]=finalAns;
-        return dp[n][i];
+        return dp[n][i]=max(includeAns,excludeAns);
     }
+    
     int cutRod(vector<int> &price) {
         // code here
         int n=price.size();
