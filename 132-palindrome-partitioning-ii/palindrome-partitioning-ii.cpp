e09b5e33ca59d43f598f9ef6eaa29d3b3f628d27
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-16 17:38:05 +0530

    Time: 1756 ms (17.80%) | Memory: 9.5 MB (54.98%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -8,25 +8,24 @@ public:
         }
         return true;
     }
-    int minCut_solver(string&s,int n,int i,vector<int>&dp){
+    int f(string&s,int n,int i,vector<int>&dp){
         // base case
         if(i==n) return 0;
+        int minAns=INT_MAX;
         if(dp[i]!=-1) return dp[i];
-        int minCost=INT_MAX;
         for(int j=i;j<n;j++){
             if(isPalindrome(i,j,s)){
-                int cost=1+minCut_solver(s,n,j+1,dp);
-                minCost=min(minCost,cost);
+                int ans=1+f(s,n,j+1,dp);
+                minAns=min(minAns,ans);
             }
-        }
-        dp[i]=minCost;
-        return dp[i];
+        } 
+        return dp[i]=minAns;
     }
     int minCut(string s) {
         int n=s.size();
         int index=0;
         vector<int>dp(n+1,-1);
-        int ans=minCut_solver(s,n,index,dp)-1;
+        int ans=f(s,n,index,dp)-1;
         return ans;
     }
 };
