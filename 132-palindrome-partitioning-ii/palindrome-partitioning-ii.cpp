36ee9bafb66490faf8d3c9019894b7c72f3667cd
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-16 17:47:54 +0530

    Time: 1769 ms (11.87%) | Memory: 9.5 MB (63.53%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -11,14 +11,14 @@ public:
     int f(string&s,int n,int i,vector<int>&dp){
         // base case
         if(i==n) return 0;
-        int minAns=INT_MAX;
         if(dp[i]!=-1) return dp[i];
+        int minAns=INT_MAX;
         for(int j=i;j<n;j++){
             if(isPalindrome(i,j,s)){
                 int ans=1+f(s,n,j+1,dp);
                 minAns=min(minAns,ans);
             }
-        } 
+        }
         return dp[i]=minAns;
     }
     int minCut(string s) {
