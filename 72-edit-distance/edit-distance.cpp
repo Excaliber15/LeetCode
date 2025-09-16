4875b6c982c497229fbdf8a3e854af61fc5718c0
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-09-16 18:56:41 +0530

    Time: 2 ms (93.13%) | Memory: 13.1 MB (48.56%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int minDistance_solver(string&word1, string&word2,int i,int j,vector<vector<int>>&dp){
+    int minDistance_helper(string&word1, string&word2,int i,int j,vector<vector<int>>&dp){
         // base cases
         if(i>word1.size()) return j+1;
         if(j>word2.size()) return i+1;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(word1[i]==word2[j]) return 0+minDistance_solver(word1,word2,i-1,j-1,dp);
-        int insert=1+minDistance_solver(word1,word2,i,j-1,dp);
-        int del=1+minDistance_solver(word1,word2,i-1,j,dp);
-        int replace=1+minDistance_solver(word1,word2,i-1,j-1,dp);
+        if(word1[i]==word2[j]) return 0+minDistance_helper(word1,word2,i-1,j-1,dp);
+        int insert=1+minDistance_helper(word1,word2,i,j-1,dp);
+        int del=1+minDistance_helper(word1,word2,i-1,j,dp);
+        int replace=1+minDistance_helper(word1,word2,i-1,j-1,dp);
         int finalAns=min(min(insert,del),replace);
         dp[i][j]=finalAns;
-        return dp[i][j];
+        return finalAns;
     }
     int minDistance(string word1, string word2) {
         int n=word1.size(),m=word2.size();
         int i=n-1,j=m-1;
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        int ans=minDistance_solver(word1,word2,i,j,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m,-1));
+        int ans=minDistance_helper(word1,word2,i,j,dp);
         return ans;
     }
 };
