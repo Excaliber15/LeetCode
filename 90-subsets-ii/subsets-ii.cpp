37730dfff9d05c3531341d8dae6d3b41ffe2d35c
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-11-04 18:02:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (84.73%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    void printAllSubsets(vector<int>& nums,vector<vector<int>>&ans,vector<int>&allSubsets,int i){
+    void printAllSubsets(vector<int>& nums,vector<vector<int>>&ans,vector<int>&v,int i){
         // base case
         if(i>=nums.size()){
-            ans.push_back(allSubsets);
+            ans.push_back(v);
             return;
         }
-        // include ans
-        allSubsets.push_back(nums[i]);
-        printAllSubsets(nums,ans,allSubsets,i+1);
-        allSubsets.pop_back();
-        // exclude ans
-        // look for repeatation
+        // include case
+        v.push_back(nums[i]);
+        printAllSubsets(nums,ans,v,i+1);
+        v.pop_back();
+        // exclude case
         int idx=i+1;
         while(idx<nums.size() && nums[idx]==nums[idx-1]) idx++;
-        printAllSubsets(nums,ans,allSubsets,idx);
+        printAllSubsets(nums,ans,v,idx);
+
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
-        vector<int>allSubsets;
+        vector<int>v;
         int index=0;
-        printAllSubsets(nums,ans,allSubsets,index);
+        printAllSubsets(nums,ans,v,index);
         return ans;
     }
 };
