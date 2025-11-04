b3566884a2383d2a7e7d13be44cd96dac0149a13
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-11-04 18:19:10 +0530

    Time: 3 ms (40.12%) | Memory: 13.9 MB (97.93%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    void combinationSum2_helper(vector<int>& candidates, int target,vector<vector<int>>&ans,vector<int>&v,int index){
+    void combinatiomSum2_helper(vector<int>& candidates, int target,vector<vector<int>>&ans,vector<int>&v,int idx){
         // base case
         if(target==0){
             ans.push_back(v);
             return;
         }
-        if(target<0) return;
-
-        for(int i=index;i<candidates.size();i++){
-            if(i>index && candidates[i]==candidates[i-1]) continue;
+        if(target<0) return ;
+        for(int i=idx;i<candidates.size();i++){
+            if(i>idx && candidates[i]==candidates[i-1]) continue;
             v.push_back(candidates[i]);
-            combinationSum2_helper(candidates,target-candidates[i],ans,v,i+1);
+            combinatiomSum2_helper(candidates,target-candidates[i],ans,v,i+1);
             v.pop_back();
         }
     }
@@ -20,7 +19,7 @@ public:
         vector<vector<int>>ans;
         vector<int>v;
         int index=0;
-        combinationSum2_helper(candidates,target,ans,v,index);
+        combinatiomSum2_helper(candidates,target,ans,v,index);
         return ans;
     }
 };
