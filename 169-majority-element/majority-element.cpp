81a56d004480cc5709575ffb96904fb18292945b
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-11-05 17:55:07 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (28.79%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // Moore's Voting Algorithm
-        int freq=0;
-        int ans=0;
+        // Moore's Voting Algoritm
+        int freq=0,ans=0;
         for(int i=0;i<nums.size();i++){
             if(freq==0) ans=nums[i];
             if(ans==nums[i]) freq++;
