9ee88857e32820df2d4752d3a312198da893b7d6
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-11-07 14:37:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (69.61%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void printAllPermutation(vector<int>& nums,vector<vector<int>>&ans,int i){
+    void printAllPermutations(vector<int>& nums,vector<vector<int>>&ans,int i){
         // base case
         if(i>=nums.size()){
             ans.push_back(nums);
@@ -8,14 +8,14 @@ public:
         }
         for(int j=i;j<nums.size();j++){
             swap(nums[i],nums[j]);
-            printAllPermutation(nums,ans,i+1);
+            printAllPermutations(nums,ans,i+1);
             swap(nums[i],nums[j]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
         int index=0;
-        printAllPermutation(nums,ans,index);
+        printAllPermutations(nums,ans,index);
         return ans;
     }
 };
