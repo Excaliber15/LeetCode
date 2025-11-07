3076f95d0d8689790f2ff1463af0ce053f0b7927
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-11-07 12:08:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Search/array-search.cpp
+++ Difficulty: Basic/Array Search/array-search.cpp
@@ -1,12 +1,9 @@
 class Solution {
   public:
     int search(vector<int>& arr, int x) {
-        // Linear Search Algo
-        int n=arr.size();
-        for(int i=0;i<n;i++){
-            if(arr[i]==x){
-                return i; // Element Found
-            }
+        // code here
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==x) return i; // Element Found
         }
         return -1; // Element Not Found
     }
