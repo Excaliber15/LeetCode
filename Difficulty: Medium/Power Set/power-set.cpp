23e0f762e5ab6c9bdb6b3fc101f3b96ceee631d4
Saurabh Sarkar <saurabhsarkar580@gmail.com>
2025-11-07 13:01:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Power Set/power-set.cpp
+++ Difficulty: Medium/Power Set/power-set.cpp
@@ -1,22 +1,22 @@
 class Solution {
   public:
-    void printAllSubsequences(string&s,vector<string>&ans,string output,int i){
+    void printAllSubsequneces(string s, vector<string>&ans,int i,string output){
         // base case
         if(i>=s.size()){
             ans.push_back(output);
             return;
         }
-        // include case
-        printAllSubsequences(s,ans,output+s[i],i+1);
-        // exclude case
-        printAllSubsequences(s,ans,output,i+1);
+        // include ans
+        printAllSubsequneces(s,ans,i+1,output+s[i]);
+        // exclude ans
+        printAllSubsequneces(s,ans,i+1,output);
     }
     vector<string> AllPossibleStrings(string s) {
         // Code here
         vector<string>ans;
-        string output="";
         int index=0;
-        printAllSubsequences(s,ans,output,index);
+        string output="";
+        printAllSubsequneces(s,ans,index,output);
         sort(ans.begin(),ans.end());
         return ans;
     }
